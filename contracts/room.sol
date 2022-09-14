ef680c6cda645e358397670ebfddb359db6ad655
Antônio Carlos <antonio@p21.local>
2022-09-14 12:00:18 -0300

    Initial constructors (token name and token room value). Room inherits Token.

--- contracts/room.sol
+++ contracts/room.sol
@@ -1,6 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract RoomToken{
-    
+import "./token.sol";
+
+contract RoomToken is Token{
+    uint256 value;
+
+    constructor(string memory _name, uint256 _value) Token(_name){
+        value = _value;
+    }
 }
--- contracts/token.sol
+++ contracts/token.sol
@@ -2,5 +2,9 @@
 pragma solidity ^0.8.0;
 
 contract Token{
-    
+    string public name;
+
+    constructor(string memory _name){
+        name = _name;
+    }
 }
