ce1a6b06a3334ec55801ae1b0459a99c3cef435b
Antônio Carlos <antonio@p21.local>
2022-09-14 16:31:53 -0300

    Token changed to ERC20 stantards. Name and total amount are now the RoomToken variables. totalSupply() implementation.

--- contracts/room.sol
+++ contracts/room.sol
@@ -3,10 +3,32 @@ pragma solidity ^0.8.0;
 
 import "./token.sol";
 
-contract RoomToken is Token{
-    uint256 value;
+contract RoomToken is ERC20Token{
+    string public name;
+    uint256 public amountOfTokens;
 
-    constructor(string memory _name, uint256 _value) Token(_name){
-        value = _value;
+    constructor(string memory _name, uint256 _amountOfTokens){
+        name = _name;
+        amountOfTokens = _amountOfTokens;
+    }
+
+    function totalSupply() external view returns (uint256){
+        return amountOfTokens;
+    }
+    function balanceOf(address account) external view returns (uint256){
+
+    }
+    function allowance(address owner, address spender) external view returns (uint256){
+
+    }
+
+    function transfer(address recipient, uint256 amount) external returns (bool){
+
+    }
+    function approve(address spender, uint256 amount) external returns (bool){
+
+    }
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool){
+        
     }
 }
