82fc54b75572e12cb18289cb8111c8bacba7b974
Antônio Carlos <antonio@p21.local>
2022-09-15 14:46:01 -0300

    Allowance isue solved on transferFrom(). balances[msg.sender] set to _totalTokens. New smart contract needed to create de token on a specific room.

--- contracts/room.sol
+++ contracts/room.sol
@@ -5,13 +5,16 @@ import "./token.sol";
 
 contract RoomToken is ERC20Token{
     string public name;
+    string public symbol;
     uint256 public totalTokens;
     mapping(address => uint256) public balances;
     mapping(address => mapping(address => uint256)) public allowances;
 
-    constructor(string memory _name, uint256 _totalTokens){
+    constructor(string memory _name, string memory _symbol, uint256 _totalTokens){
         name = _name;
+        symbol = _symbol;
         totalTokens = _totalTokens;
+        balances[msg.sender] = _totalTokens;
     }
 
     function totalSupply() public override view returns (uint256){
@@ -42,10 +45,11 @@ contract RoomToken is ERC20Token{
         return true;
     }
     function transferFrom(address sender, address recipient, uint256 amount) public override returns (bool){
-        require(amount <= balances[msg.sender]);
+        require(amount <= balances[sender]);
         
         balances[sender] = balances[sender] - amount;
         balances[recipient] = balances[recipient] + amount;
+        allowances[sender][msg.sender] = allowances[sender][msg.sender] - amount;
         emit Transfer(sender, recipient, amount);
         
         return true;
