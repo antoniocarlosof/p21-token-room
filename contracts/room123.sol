e3fbba803a62f70e5a185741a818925d7544f00c
Antônio Carlos <antonio@p21.local>
2022-09-15 14:58:31 -0300

    Room123 set on it's own constructor. Functions buy() and sell() declared().

--- contracts/room123.sol
+++ contracts/room123.sol
@@ -4,5 +4,13 @@ pragma solidity ^0.8.0;
 import "./room.sol";
 
 contract Room123 is RoomToken{
-    RoomToken room123 = new RoomToken("room123", "R123", 100);
+    RoomToken public room123;
+
+    constructor(){
+        room123 = new RoomToken("room123", "R123", 100);
+    }
+
+    function buy() payable public{
+
+    }
 }
