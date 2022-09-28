365e82bdb190a220c79670d55ee5824356f27664
Antônio Carlos <antonio.carlos.o.ferreira@gmail.com>
2022-09-28 10:09:02 -0300

    On buy(), the tokens being transfered were changed from all tokens of an offer to the amount of tokens the user wants to buy. offerList update implemented.

--- contracts/room123.sol
+++ contracts/room123.sol
@@ -21,6 +21,7 @@ contract Room123{
         tokenCount = 0;
         room123 = new RoomToken("room123", "R123", 100);
         offerList.push(tokenOffer(tokenCount, address(this), room123.totalSupply(), 5));
+        amountOffered[msg.sender] = room123.totalSupply();
     }
 
     function allowSystem(uint256 amount) public{
@@ -31,12 +32,13 @@ contract Room123{
 
     function buy(uint id, uint amountToBuy) payable public{
         require(amountToBuy > 0);
-        // Must implement update on offerList after transaction
         tokenOffer memory offerToBuy = offerList[id];
         uint finalPrice = offerToBuy.pricePerToken * amountToBuy;
         require(payable(offerToBuy.owner).send(finalPrice));
-        room123.transferFrom(offerToBuy.owner, msg.sender, offerToBuy.amountOfTokens);
+        room123.transferFrom(offerToBuy.owner, msg.sender, amountToBuy);
         amountOffered[offerToBuy.owner] = amountOffered[offerToBuy.owner] - amountToBuy;
+        offerToBuy.amountOfTokens = offerToBuy.amountOfTokens - amountToBuy;
+        offerList[id] = offerToBuy;
     }
 
     function offer(uint amount, uint256 paymentWei) public{
