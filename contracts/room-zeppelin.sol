76e2a205fb62c00137e8b3aa5418dd170f3e1112
Antônio Carlos <antonio.carlos.o.ferreira@gmail.com>
2022-09-30 14:58:34 -0300

    Offer variables implemented.

--- contracts/room-zeppelin.sol
+++ contracts/room-zeppelin.sol
@@ -5,9 +5,24 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Room1412 is ERC20 {
     address public admin;
+    uint256 offerId;
+
+    struct tokenOffer{
+        address payable owner;
+        uint256 amountOfTokens;
+        uint256 pricePerToken;
+    }
+
+    mapping(uint256 => tokenOffer) public offerList;
+    mapping(address => uint256) public amountOffered;
 
     constructor() ERC20("Room1412", "R12") {
+        offerId = 0;
+
         _mint(msg.sender, 100);
+        offerList[offerId] = tokenOffer(payable(msg.sender), totalSupply(), 5000);
+        amountOffered[msg.sender] = totalSupply();
+        
         admin = msg.sender;
     }
 }
