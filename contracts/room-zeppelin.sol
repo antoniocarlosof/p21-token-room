f8edf18ecf64b3bb255c836e5f7f3f9043d8e6c0
Antônio Carlos <antonio.carlos.o.ferreira@gmail.com>
2022-09-30 16:23:29 -0300

    buy() under implementation. Requirements implemented but not tested.

--- contracts/room-zeppelin.sol
+++ contracts/room-zeppelin.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Room1412 is ERC20 {
     address public admin;
-    uint256 offerId;
+    uint256 offerCount;
 
     struct tokenOffer{
         address payable owner;
@@ -17,12 +17,20 @@ contract Room1412 is ERC20 {
     mapping(address => uint256) public amountOffered;
 
     constructor() ERC20("Room1412", "R12") {
-        offerId = 0;
+        offerCount = 0;
 
         _mint(msg.sender, 100);
-        offerList[offerId] = tokenOffer(payable(msg.sender), totalSupply(), 5000);
+        offerList[offerCount] = tokenOffer(payable(msg.sender), totalSupply(), 5000);
         amountOffered[msg.sender] = totalSupply();
         
         admin = msg.sender;
+    }
+
+    function buy(uint256 _offerId, uint256 amountToBuy) payable public {
+        require(amountToBuy > 0, "Desired amount of tokens must be greater than 0");
+        
+        tokenOffer memory offerToBuy = offerList[_offerId];
+        bool transaction = offerToBuy.owner.send(msg.value);
+        require(transaction, "ETH transaction failure");
     }
 }
